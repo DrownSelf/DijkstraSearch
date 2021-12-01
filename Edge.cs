d107d68d000505f5ec376578e00d658bf29f5888
DrownSelf <strigelskiy.petr@gmail.com>
2021-12-01 15:53:15 +0300

    init

---
+++ Edge.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    class Graph
+    {
+        public Dictionary<int, Dictionary<int, int>> neighbours;
+
+        public Dictionary<int, int> ways;
+        
+
+        private bool CheckVisited(int vertex, List<int> visited)
+        {
+            foreach(int compare in visited)
+                if (compare == vertex)
+                    return false;
+            return true;
+        }
+
+        public Graph() 
+        {
+            neighbours = new Dictionary<int, Dictionary<int, int>>();
+            ways = new Dictionary<int, int>();
+            
+        }
+        
+        private void DiikstraSearch(int begin)
+        {
+            Queue<int> q = new Queue<int>();
+            List<int> visited = new List<int>();
+            int path = 0;
+            q.Enqueue(begin);
+            while (q.Count != 0)
+            {
+                int extracted = q.Peek();
+                int min = int.MaxValue;
+                int finding = int.MaxValue;
+                path = ways[extracted];
+                foreach (KeyValuePair<int, int> compared in neighbours[extracted])
+                {
+                    if (compared.Value <= min && CheckVisited(compared.Key, visited))
+                    {
+                        min = compared.Value;
+                        finding = compared.Key;
+                    }
+                    if (compared.Value + path < ways[compared.Key])
+                        ways[compared.Key] = compared.Value + path;
+                }
+                if(finding!=int.MaxValue)
+                    q.Enqueue(finding);
+                q.Dequeue();
+                visited.Add(extracted);
+            } 
+        }
+
+        public void ShowPath(int begin)
+        {
+            DiikstraSearch(begin);
+            foreach (KeyValuePair<int, int> elements in ways)
+            {
+                Console.WriteLine("Вершина: {0} = {1}", elements.Key, elements.Value);
+            }
+        }
+
+        public void Creating(int vertex, int neighbour, int path)
+        {
+            if(!neighbours.ContainsKey(vertex))
+            {
+                Dictionary<int, int> Second = new Dictionary<int, int>();
+                Second.Add(neighbour, path);
+                neighbours.Add(vertex, Second);
+            }
+            else
+                neighbours[vertex].Add(neighbour, path);
+        }
+
+        public void AddConnection(int vertex, int neighbour, int path)
+        {
+            Creating(vertex, neighbour, path);
+            Creating(neighbour, vertex, path);
+        }
+
+        public void Reset(int begin)
+        {
+            foreach (KeyValuePair<int, Dictionary<int, int>> vertexes in neighbours)
+            {
+                if (vertexes.Key == begin)
+                    ways[vertexes.Key] = 0;
+                else
+                 ways[vertexes.Key] = int.MaxValue;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph Five = new Graph();
+            Five.AddConnection(1, 2, 7);
+            Five.AddConnection(1, 3, 9);
+            Five.AddConnection(1, 6, 14);
+            Five.AddConnection(2, 3, 10);
+            Five.AddConnection(2, 4, 15);
+            Five.AddConnection(4, 3, 11);
+            Five.AddConnection(3, 6, 2);
+            Five.AddConnection(6, 5, 9);
+            Five.AddConnection(4, 5, 6);
+            Five.Reset(1);
+            Five.ShowPath(1);
+        }
+    }
+}
